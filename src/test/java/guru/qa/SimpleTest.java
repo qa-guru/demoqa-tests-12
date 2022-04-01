cd0bab555c1c8917b531de7f1ad5434950c0d375
Dmitrii Tuchs <dtuchs@gmail.com>
2022-04-01 21:05:00 +0300

    JUnit 5

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,54 @@
+package guru.qa;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+
+    @BeforeAll
+    static void initDB() {
+        System.out.println("### @BeforeAll");
+    }
+
+    @BeforeEach
+    void openYaPage() {
+        System.out.println("###     @BeforeEach");
+//        Selenide.open("https://ya.ru");
+    }
+
+    @AfterEach
+    void close() {
+        System.out.println("###     @AfterEach");
+//        WebDriverRunner.closeWindow();
+    }
+
+    @AfterAll
+    static void cleanDB() {
+        System.out.println("### @AfterAll");
+    }
+
+    @Test
+    void assertTest() {
+        System.out.println("###             @Test 0");
+        // ... поиск в новостях
+    }
+
+    @Test
+    void assertTest1() {
+        System.out.println("###             @Test 1");
+        // ... поиск в картинках
+    }
+
+    @Test
+    void assertTest2() {
+        System.out.println("###             @Test 2");
+        // ... поиск в новостях
+    }
+
+    @Test
+    void assertTest3() {
+        System.out.println("###             @Test 3");
+        // ... поиск в картинках
+    }
+}
